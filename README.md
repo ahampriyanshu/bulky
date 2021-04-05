178ba7541f56ff0b5c4fa73cd81add2613d2fb11
ahampriyanshu <ahampriyanshu@gmail.com>
2021-04-05 16:04:38 +0530

    98% completed

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-<p align="center"><img width="128" height="128" src="https://user-images.githubusercontent.com/54521023/113477687-3123b000-94a1-11eb-9120-71e33c2664d9.png"></p>
+<p align="center"><img width="128" height="128" src="https://raw.githubusercontent.com/ahampriyanshu/bulky/master/asset/img/logo.png"></p>
 
-<h1 align="center">Bulky</h1>
 
 [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://bulky.vercel.app/)
 [![Open Source Love svg2](https://badges.frapsoft.com/os/v2/open-source.svg?v=103)](https://bulky.vercel.app/)
 [![size](https://img.shields.io/github/repo-size/ahampriyanshu/ojas?style=flat-square)](https://bulky.vercel.app/)
 [![Website status](https://img.shields.io/website-up-down-green-red/http/shields.io.svg)](https://bulky.vercel.app/)
-
-
 
 ## Desktop view
 <p align="center" >
@@ -22,13 +19,11 @@
 
 ## How it works
 
-* Upload a .csv file contating **Participant, Title,Organisation, Event, Sign, Name, Designation, Layout** ( [click here]() to download demo file )
-* Or fill up [this](https://bulky.vercel.app/#demoForm) form to try a demo instead.
+* [Click here](https://raw.githubusercontent.com/ahampriyanshu/bulky/master/asset/sample.csv) to download sample .csv file => Enter data in correct sequence => Upload .csv => Download .pdf
+* Or fill up [this](https://bulky.vercel.app/#demoForm) form to create single pdf instead.
 
 ## Dependencies
 
 * [pdf-lib](https://pdf-lib.js.org/)
 * [FileSaver.js](https://github.com/eligrey/FileSaver.js/)
-* [Tailwind CSS](https://tailwindcss.com/)
-* [Google Font](https://fonts.google.com/)
-* [JSZip](https://stuk.github.io/jszip/)
+* [Tailwindcss](https://tailwindcss.com/)
--- sw.js
+++ sw.js
@@ -1,39 +1,131 @@
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'index.html';
+const staticCacheName = 'site-static-v1';
+const dynamicCacheName = 'site-dynamic-v1';
+const assets = [
+  './',
+  './index.html',
+  './asset/fallback.html',
+  './asset/js/FileSaver.js',
+  './asset/js/index.js',
+  './asset/js/pdf-lib@1.4.0',
+  './asset/js/parse.js',
+  './asset/js/fontkit@0.0.4',
+  './asset/css/custom-forms.min.css',
+  './asset/css/tailwind.min.css',
+  './asset/css/style.css',
+  './asset/sample.csv',
+  './asset/sign.jpg',
+  'asset/img/apple-icon-60x60.png',
+  'asset/img/apple-icon-72x72.png',
+  'asset/img/apple-icon-76x76.png',
+  'asset/img/apple-icon-114x114.png',
+  'asset/img/apple-icon-120x120.png',
+  'asset/img/apple-icon-144x144.png',
+  'asset/img/apple-icon-152x152.png',
+  'asset/img/apple-icon-180x180.png',
+  'asset/img/android-icon-192x192.png',
+  'asset/img/favicon-32x32.png',
+  'asset/img/favicon-96x96.png',
+  'asset/img/favicon-16x16.png',
+  'favicon.ico',
+  './asset/font/BebasNeue-Regular.ttf',
+  'asset/img/ms-icon-144x144.png',
+  'asset/img/logo.png',
+  'asset/sample_result/1.png',
+  'asset/sample_result/2.png',
+  'asset/sample_result/2.png',
+  'asset/sample_result/3.png',
+  'asset/sample_result/4.png',
+  'asset/sample_result/5.png',
+  'asset/sample_result/6.png',
+  'asset/sample_result/7.png',
+  'asset/sample_result/8.png',
+  'asset/sample_result/9.png',
+  'asset/sample_result/10.png',
+  'asset/sample_result/11.png',
+  'asset/sample_result/12.png',
+  'asset/sample_result/13.png',
+  'asset/sample_result/14.png',
+  'asset/sample_result/15.png',
+  'asset/sample_result/16.png',
+  'asset/sample_result/17.png',
+  'asset/sample_result/18.png',
+  'asset/sample_result/19.png',
+  'asset/sample_result/20.png',
+  'asset/sample_result/21.png',
+  'asset/sample_result/22.png',
+  'asset/sample_result/23.png',
+  'asset/sample_result/24.png',
+  'asset/sample_layout/layout_1.png',
+  'asset/sample_layout/layout_2.png',
+  'asset/sample_layout/layout_2.png',
+  'asset/sample_layout/layout_3.png',
+  'asset/sample_layout/layout_4.png',
+  'asset/sample_layout/layout_5.png',
+  'asset/sample_layout/layout_6.png',
+  'asset/sample_layout/layout_7.png',
+  'asset/sample_layout/layout_8.png',
+  'asset/sample_layout/layout_9.png',
+  'asset/sample_layout/layout_10.png',
+  'asset/sample_layout/layout_11.png',
+  'asset/sample_layout/layout_12.png',
+  'asset/sample_layout/layout_13.png',
+  'asset/sample_layout/layout_14.png',
+  'asset/sample_layout/layout_15.png',
+  'asset/sample_layout/layout_16.png',
+  'asset/sample_layout/layout_17.png',
+  'asset/sample_layout/layout_18.png',
+  'asset/sample_layout/layout_19.png',
+  'asset/sample_layout/layout_20.png',
+  'asset/sample_layout/layout_21.png',
+  'asset/sample_layout/layout_22.png',
+  'asset/sample_layout/layout_23.png',
+  'asset/sample_layout/layout_24.png',
+];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-  self.clients.claim();
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      )
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('Fetch failed; returning offline page instead.', error);
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
       }
-    })());
-  }
+    });
+  });
+};
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          limitCacheSize(dynamicCacheName, 10);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if (evt.request.url.indexOf('.html') > -1) {
+        return caches.match('./asset/fallback.html');
+      }
+    })
+  );
 });
